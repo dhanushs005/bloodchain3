4786a444fd05f3fcf2f00ffb1cf4088b31bde580
dhanushs005 <144521278+dhanushs005@users.noreply.github.com>
2025-02-23 20:59:16 +0530

    Update settings.py

--- onlinebb/settings.py
+++ onlinebb/settings.py
@@ -119,12 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Image)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Where BCview.apk lives during development
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Media files (User uploads)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
